5c5b8156e9f71556bbf6f3903f422f040b817162
Samy Bencherif <samybencherif@live.com>
2019-08-06 02:01:55 -0400

    frogs doing stuff

--- src/examples/frogs.c
+++ src/examples/frogs.c
@@ -18,8 +18,16 @@ int random_int(int x)
   return rand() % x;
 }
 
+float distance(float x1, float y1, float x2, float y2)
+{
+  return sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+}
+
 component* frog1;
 component* frog2;
+
+component* tongue1;
+component* tongue2;
 
 float fly_vx = 0;
 float fly_vy = 0;
@@ -42,6 +50,14 @@ void setup()
 
   /* matrix hack, flips horizontally */
   frog2->transform->rot_scale->m00 *= -1;
+
+  tongue1 = component_add(quad_new(0,0,1,2,RED));
+  tongue1->transform->translate->x = 16;
+  tongue1->transform->translate->y = 5;
+
+  tongue2 = component_add(quad_new(0,0,1,2,RED));
+  tongue2->transform->translate->x = -24;
+  tongue2->transform->translate->y = 0;
 
   fly = component_add(bitmap_from_file("tex/fly2.kbm", NULL));
   flyFrames[1] = *(bitmap*)fly->fields;
@@ -92,6 +108,20 @@ void update()
   {
     fly_vx = 1;
   }
+
+  transform_set_angle(tongue1->transform, atan2(
+        4 + fly->transform->translate->y - 5,
+        1 + fly->transform->translate->x - 16
+        ));
+  ((quad*)tongue1->fields)->w = distance(16, 5,
+    fly->transform->translate->x+1, fly->transform->translate->y+4);
+
+  transform_set_angle(tongue2->transform, atan2(
+        4 + fly->transform->translate->y - 0,
+        1 + fly->transform->translate->x + 24
+        ));
+  ((quad*)tongue2->fields)->w = distance(-24, 0,
+    fly->transform->translate->x+1, fly->transform->translate->y+4);
 }
 
 void key(char k)
