cd88bd94e5b46d328aa227ca452da1158c86c8c9
Samy Bencherif <samybencherif@live.com>
2019-08-19 23:17:46 -0400

    trades

--- examples/valley-of-crystals/src/main.c
+++ examples/valley-of-crystals/src/main.c
@@ -2,7 +2,7 @@
 /* LIB */
 
 #include <stdlib.h>
-
+#include <string.h>
 #include "ncknge.h"
 
 #include <math.h>
@@ -25,6 +25,14 @@ scene present;
 /* DATA */
 
 int t = 0;
+
+/* HELPER FUNCTIONS */
+
+/* print 1-line of centered text */
+void printc(int x, int y, color c, char* msg)
+{
+    print(x - strlen(msg)/2, y, c, msg);
+}
 
 /* called before each scene */
 void init()
@@ -65,7 +73,7 @@ void update()
     {
         case title:
 
-        print(W/2 - 14, H/2 + 6, BLACK, "~ Press Any Key To Begin ~");
+        printc(W/2, H/2 + 6, BLACK, "~ Press Any Key To Begin ~");
 
         if (t%5 == 0)
         {
@@ -75,9 +83,14 @@ void update()
 
         break;
         case identity:
-        print(0,0,BLACK,"Choose an identity.");
-        print(0,2,BLACK,"I am a loser.");
-        print(0,3,WHITE,"I am a cool guy.");
+        printc(W/2, H/2-8, BLACK, "First, choose your identity.");
+        printc(W/2, H/2-6, BLACK, "What is your skillset?");
+
+        print(W/2-8, H/2-4, WHITE, "Friendly");
+        print(W/2-8, H/2-3, BLACK, "Swiftness");
+        print(W/2-8, H/2-2, BLACK, "Bargaining");
+        print(W/2-8, H/2-1, BLACK, "Courage");
+        print(W/2-8, H/2, BLACK, "Enchanting");
         break;
         default:
         break;
